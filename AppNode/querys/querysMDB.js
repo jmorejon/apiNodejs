3ff285b5d992076dd24839c305fae6adff34dac5
WilderSiguantay <201113769@ingenieria.usac.edu.gt>
2020-07-05 20:38:05 -0600

    mongo
    mongo

--- AppNode/querys/querysMDB.js
+++ AppNode/querys/querysMDB.js
@@ -10,4 +10,15 @@ db.COVID.aggregate(
 
 //todo   
 db.COVID.find()
+
+
+//total por edades   
    
+db.COVID.aggregate(
+    {"$group" : { "_id": "Edad", "count": { "$sum": 1 } } },
+    {"$match": {"_id" :{ "$ne" : null } , "count" : {"$gt": 1} } }, 
+    {"$sort": {"count" : -1} },
+    {"$project": {"Edad" : "$_id", "_id" : 0} },
+).limit(3)
+
+db.COVID.count()
